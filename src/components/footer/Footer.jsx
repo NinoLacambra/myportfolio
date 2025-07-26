34ca5e1ad2413b08c4e59ad7f41e5818a09d6516
Niño Catalan Lacambra <132426184+NinoLacambra@users.noreply.github.com>
2025-07-26 21:30:16 +0800

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -58,7 +58,7 @@ const Footer = () => {
 
         {/* Copyright Text */}
         <p className="text-sm text-gray-400 mt-6">
-          © 2025 Tarun Kaushik. All rights reserved.
+          © 2025 Niño Lacambra. All rights reserved.
         </p>
       </div>
     </footer>
